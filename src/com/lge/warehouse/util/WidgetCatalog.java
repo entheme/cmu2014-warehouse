564fdd5f34cc4e4f60b4be95cf539d2a8a063eb3
seuki77 <seuki77@gmail.com>
2014-06-22 12:44:34 +0900

    WidgetCatalog.java

--- src/com/lge/warehouse/util/WidgetCatalog.java
+++ src/com/lge/warehouse/util/WidgetCatalog.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -26,56 +25,3 @@ public class WidgetCatalog implements Serializable{
             return mWidgetList.get(index);
         }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package com.lge.warehouse.util;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- *
- * @author kihyung2.lee
- */
-public class WidgetCatalog {
-    static private final HashMap<Integer, Widget> sMapInt2Widget;
-    static private final HashMap<Class, Integer> sMapClass2Int;
-
-    static {
-        sMapInt2Widget = new HashMap<>();
-        sMapClass2Int = new HashMap<>();
-
-        addToCatalog(new SoccerBallWidget(0));
-        addToCatalog(new YellowBaseBallWidget(1));
-        addToCatalog(new WhiteBaseBallWidget(2));
-        addToCatalog(new BasketBallWidget(3));
-        addToCatalog(new FootBallWidget(4));
-    }
-    
-    static private void addToCatalog(Widget w) {
-        sMapInt2Widget.put(w.getProuctId(), w);
-        sMapClass2Int.put(w.getClass(), w.getProuctId());
-    }
-       
-    public static int getProductIdOf(Class cls) {
-        return sMapClass2Int.get(cls);
-    }
-    
-    public static Widget getWidgetById(int productId) {
-        return sMapInt2Widget.get(productId);
-    }
-    
-    public static Widget getWidgetByClass(Class cls) {
-        return getWidgetById(getProductIdOf(cls));
-    }
-    
-    public static int getSize(){
-        return sMapInt2Widget.size();
-    }
-}
->>>>>>> FETCH_HEAD
