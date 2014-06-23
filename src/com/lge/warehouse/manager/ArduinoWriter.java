39b6ea43ed18744a496f6190be4097b51b121181
jjab <gunho.lee@lge.com>
2014-06-23 03:12:34 -0400

    Add Input/Output Manager Class for Arduino

---
+++ src/com/lge/warehouse/manager/ArduinoWriter.java
@@ -0,0 +1,53 @@
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class ArduinoWriter {
+	
+	private String targetIP		= null;		// IP address of Ardunio
+	private Socket clientSocket = null;		// The socket.
+	private int portNum			= 504;		// Port number of socket write
+	BufferedWriter socketWriter	= null;		// Socket output to server
+
+	public ArduinoWriter(String ipaddr) {
+		
+		this.targetIP = ipaddr;
+	}
+
+	public boolean writeData(String cmd)
+ 	{
+		if (cmd == null)
+		{
+			System.out.println("Invalid command for arduino..."+cmd);
+			return false;
+		}
+		
+		if (clientSocket == null)
+		{
+			try	
+	   		{
+	   			clientSocket = new Socket(targetIP, portNum);
+	   			socketWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+			} catch (Exception e) {
+				System.err.println( "Socket Error::" + e);
+				return false;
+			}
+			
+			try {
+				socketWriter.write(cmd, 0, cmd.length());
+				socketWriter.flush();
+				
+				System.out.println("Disconnect...");
+				socketWriter.close();
+				clientSocket.close();
+				clientSocket = null;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return true;
+ 	}
+}
---
+++ src/com/lge/warehouse/manager/ArdunioReader.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class ArdunioReader {
+	
+	private ServerSocket serverSocket	= null;
+	private Socket clientSocket			= null;		// The socket.
+	private int portNum					= 505;		// Port number for socket read
+	
+	private String ardData				= null;
+
+	public ArdunioReader() {
+		
+	}
+	
+	public boolean startServer() {
+		try
+		{
+    		serverSocket = new ServerSocket(portNum);
+    		System.out.println ( "\n\nWaiting for Arduino on port " + portNum + "." );
+    	}
+		catch (IOException e)
+    	{
+    		System.err.println( "\n\nCould not instantiate socket on port: " + portNum );
+    		return false;
+    	}
+		
+		return true;
+	}
+
+	public String readData()
+ 	{
+		ardData = null;
+		
+		if (serverSocket == null)
+		{
+			System.out.println("Error:: Server is not running...");
+			return null;
+		}
+		
+		try
+		{
+    		clientSocket = serverSocket.accept();
+    	}
+		catch (Exception e)
+    	{
+    		System.err.println("Accept failed.");
+    		return null;
+    	}
+		
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader( new InputStreamReader( clientSocket.getInputStream()));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		try
+	    {
+			//while (in.ready() == false)
+			//	Thread.sleep(50);
+			ardData = in.readLine();
+	    	//System.out.println ("Data from Arduino: " + ardData);
+		} catch (Exception e) {
+			System.err.println("readLine failed::");
+    		return null;
+		}
+		
+		// To DO
+		getCmdFromData(ardData);
+		
+		return ardData;
+ 	}
+	
+	private int getCmdFromData(String str) {
+		return 0;
+	}
+}
